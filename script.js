97aab0551bd977345e904f6c6518595401406698
Aya-Fujishiro <tengdaicai@gmail.com>
2026-02-25 22:00:42 +0900

    フォームバリデーション修正と色変更機能追加

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+
 const form = document.getElementById("contact-form");
 const message = document.getElementById("message");
 
@@ -6,14 +9,16 @@ form.addEventListener("submit", function(event) {
 
     const name = form.elements["name"].value.trim();
     const email = form.elements["email"].value.trim();
-    const messeage = form.elements["message"].value.trim();
+    const messeageText = form.elements["message"].value.trim();
 
     if (!name || !email || !messageText) {
         message.textContent = "すべて入力してください";
-        message.styyle.color = "red";
+        message.style.color = "red";
     } else {
     message.textContent = "送信しました";
     message.style.color = "green";
     form.reset();
     }
+});
+
 });
