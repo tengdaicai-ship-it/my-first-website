8102af56034959f0adc5d16a0c2c775611abf002
Aya-Fujishiro <tengdaicai@gmail.com>
2026-02-25 21:42:34 +0900

    フォーム処理の改善（リセットと色変更）

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ form.addEventListener("submit", function(event) {
     const email = form.elements["email"].value.trim();
     const messeage = form.elements["message"].value.trim();
 
-    if (!name || !email || !message) {
+    if (!name || !email || !messageText) {
         message.textContent = "すべて入力してください";
+        message.styyle.color = "red";
     } else {
     message.textContent = "送信しました";
+    message.style.color = "green";
+    form.reset();
     }
 });
