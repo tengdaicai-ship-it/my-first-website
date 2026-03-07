b09d4ac13229f57d15b430355fd9080f9452db2d
Aya-Fujishiro <tengdaicai@gmail.com>
2026-03-07 16:42:27 +0900

    feat: connect delete button to DELETE API

--- public/admin.js
+++ public/admin.js
@@ -25,6 +25,16 @@ async function loadMessages() {
         const button = document.createElement("button");
         button.textContent = "Delete";
 
+        button.addEventListener("click", async () => {
+
+            await fetch(`/api/messages/${message.id}`, {
+                method: "DELETE"
+            });
+
+            loadMessages();
+
+        });
+
         del.appendChild(button);
 
         tr.appendChild(name);
